e31ca31e1cb960464a7f9e9e13efe563c89425df
ahmedabdoaglan <102321593+ahmedabdoaglan@users.noreply.github.com>
2025-06-21 20:31:43 +0300

    part 3 setting up redux toolkit

--- ecommerce-front/src/components/ecommerce/Product/Product.tsx
+++ ecommerce-front/src/components/ecommerce/Product/Product.tsx
@@ -1,20 +1,16 @@
+import type { TProduct } from "@customTypes/product";
 import { Button } from "react-bootstrap";
 import styles from "./styles.module.css";
 const { product, productImg } = styles;
 
-const Product = () => {
+const Product = ({ title, price, img }: TProduct) => {
   return (
     <div className={product}>
       <div className={productImg}>
-        <img
-          //   src="https://eg.hm.com/assets/styles/HNM/14482498/6103a8463876770c30cdba3535b7be1f333315fe/2/image-thumb__3464789__product_listing/cb91f8f128ac2125e0ec3a008a2e8d2497d15434.jpg"
-          //   alt=""
-          src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=300&h=400&fit=crop"
-          alt="Product Test"
-        />
+        <img src={img} alt={title} />
       </div>
-      <h2>Title</h2>
-      <h3>10 EGP</h3>
+      <h2>{title}</h2>
+      <h3>{price} EGP</h3>
       <Button variant="info" style={{ color: "white" }}>
         Add to cart
       </Button>
